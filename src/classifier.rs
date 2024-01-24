ade64ab7820e7e5460cda69aae160f58fcef437d
Theredwiking <hjulmandn@gmail.com>
2024-01-24 21:42:33 +0100

    Added ip version to struct

--- src/classifier.rs
+++ src/classifier.rs
@@ -6,6 +6,7 @@ use crate::listener::PacketInfo;
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct PacketConvert {
+    ipv: Box<str>,
     source_ip: String,
     dest_ip: String,
     protocol: String,
@@ -18,6 +19,7 @@ pub fn ip_version(packet: PacketInfo) {
             EtherTypes::Ipv4 => {
                 let ipv4_packet = Ipv4Packet::new(ethernet_packer.payload()).unwrap();
                 let converted = PacketConvert {
+                    ipv: "4".into(),
                     source_ip: ipv4_packet.get_source().to_string(),
                     dest_ip: ipv4_packet.get_destination().to_string(),
                     protocol: ipv4_packet.get_next_level_protocol().to_string(),
@@ -28,6 +30,7 @@ pub fn ip_version(packet: PacketInfo) {
             EtherTypes::Ipv6 => {
                 let ipv6_packet = Ipv6Packet::new(ethernet_packer.payload()).unwrap();
                 let converted = PacketConvert {
+                    ipv: "6".into(),
                     source_ip: ipv6_packet.get_source().to_string(),
                     dest_ip: ipv6_packet.get_destination().to_string(),
                     protocol: ipv6_packet.get_next_header().to_string(),
