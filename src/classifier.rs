1dc9e8c9a28d604a8bb5d6a0634c80c595a8cd95
Theredwiking <hjulmandn@gmail.com>
2024-01-24 21:40:18 +0100

    Add ipv6 and fixed warning

--- src/classifier.rs
+++ src/classifier.rs
@@ -1,11 +1,15 @@
-use pnet::packet::{ethernet::{EtherTypes, EthernetPacket}, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, Packet};
+use std::u8;
+
+use pnet::packet::{ethernet::{EtherTypes, EthernetPacket}, ipv4::Ipv4Packet, ipv6::Ipv6Packet, Packet};
 use crate::listener::PacketInfo;
 
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct PacketConvert {
     source_ip: String,
     dest_ip: String,
     protocol: String,
+    payload: Box<[u8]>,
 }
 
 pub fn ip_version(packet: PacketInfo) {
@@ -17,6 +21,17 @@ pub fn ip_version(packet: PacketInfo) {
                     source_ip: ipv4_packet.get_source().to_string(),
                     dest_ip: ipv4_packet.get_destination().to_string(),
                     protocol: ipv4_packet.get_next_level_protocol().to_string(),
+                    payload: ipv4_packet.payload().into(),
+                };
+                println!("{:?}", converted)
+            },
+            EtherTypes::Ipv6 => {
+                let ipv6_packet = Ipv6Packet::new(ethernet_packer.payload()).unwrap();
+                let converted = PacketConvert {
+                    source_ip: ipv6_packet.get_source().to_string(),
+                    dest_ip: ipv6_packet.get_destination().to_string(),
+                    protocol: ipv6_packet.get_next_header().to_string(),
+                    payload: ipv6_packet.payload().into(),
                 };
                 println!("{:?}", converted)
             },
