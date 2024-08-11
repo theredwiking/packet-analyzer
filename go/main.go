b344a8875fff22b46779c1dbe836c193bf3a68dc
Theredwiking <hjulmandn@gmail.com>
2024-08-11 13:07:16 +0200

    Can now list all interfaces

--- go/main.go
+++ go/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/theredwiking/packet-analyzer/go/core"
 )
 
 func main() {
-	core.InterfaceList()
+	interfaces := flag.Bool("i", false, "List avaible interfaces")
+	
+	flag.Parse()
+
+	if *interfaces {
+		core.InterfaceList()
+	}
 }
